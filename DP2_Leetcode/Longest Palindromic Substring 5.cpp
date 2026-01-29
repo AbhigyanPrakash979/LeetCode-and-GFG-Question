3da70bb2beaf9836a6dc34e5901e91fd99f78186
Abhigyan Prakash <abhigyanprakash979@gmail.com>
2026-01-29 21:36:38 +0530

    Practice Questions

--- DP2_Leetcode/Longest Palindromic Substring 5.cpp
+++ DP2_Leetcode/Longest Palindromic Substring 5.cpp
@@ -1,3 +1,49 @@
+// Method 1 
+class Solution {
+public:
+    // Check if substring s[i..j] is palindrome
+    bool isPalindrome(string& s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false; // mismatch found
+            }
+            i++;
+            j--;
+        }
+        return true; // no mismatches, it's a palindrome
+    }
+
+    void findLongest(string& s, int i, int& maxLen, int& start) {
+        int n = s.length();
+        if (i >= n)
+            return; // base case
+
+        // Instead of inner loop, recursion explores substrings
+        for (int j = i; j < n; j++) {
+            if (isPalindrome(s, i, j)) {
+                if (j - i + 1 > maxLen) {// yeh check krega ki updatekrna hai ki nhi
+                    maxLen = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+
+        // recursive call for next starting index
+        findLongest(s, i + 1, maxLen, start);
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int maxLen = INT_MIN;
+        int start = 0;
+
+        findLongest(s, 0, maxLen, start);
+
+        return s.substr(start, maxLen);
+    }
+};
+
+//Method 2
 class Solution {
 public:
     bool solve(string& s, int i, int j) {
